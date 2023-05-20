c6cb71cb83850089c915fa68a06f27c601aec9cd
Anita <anitaghanata@gmail.com>
2023-05-20 11:19:27 +0530

    Whatsapp

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -2,6 +2,31 @@ package com.driver;
 
 public class Group {
     private String name;
+
     private int numberOfParticipants;
+
+    public Group(){
+
+    }
+    public Group(String name,int num){
+        this.name = name;
+        this.numberOfParticipants = num;
+    }
+
+    //Getters and Setters
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getNumberOfParticipants(){
+        return this.numberOfParticipants;
+    }
+    public void setNumberOfParticipants(int num){
+        this.numberOfParticipants = num;
+    }
 
 }
